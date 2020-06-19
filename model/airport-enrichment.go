f9f040145d73b9f33c85fb203d1a4069809898b2
Tarif Halabi <tarif.halabi@gmail.com>
2020-06-18 22:56:16 -0400

    Better error handling.

--- model/airport-enrichment.go
+++ model/airport-enrichment.go
@@ -34,6 +34,7 @@ func ClearRows() {
 	sqlStatement := "truncate table airport_enrichment reuse storage"
 	_, error := Db.Exec(sqlStatement)
 	if error != nil {
+		log.Println(error)
 		log.Fatal("Failed to execute sql ", sqlStatement)
 	}
 
@@ -52,7 +53,15 @@ func PersistRows(columnNameToIndex map[string]int, rows [][]string) {
 			continue
 		}
 
-		tx.MustExec(sqlStatement, airportEnrichment.ID, airportEnrichment.Ident, airportEnrichment.Type, airportEnrichment.Name, airportEnrichment.LatitudeDeg, airportEnrichment.LongitudeDeg, airportEnrichment.ElevationFt, airportEnrichment.Continent, airportEnrichment.IsoCountry, airportEnrichment.IsoRegion, airportEnrichment.Municipality, airportEnrichment.ScheduledService, airportEnrichment.GpsCode, airportEnrichment.IataCode, airportEnrichment.LocalCode, airportEnrichment.HomeLink, airportEnrichment.WikipediaLink, airportEnrichment.Keywords)
+		result := tx.MustExec(sqlStatement, airportEnrichment.ID, airportEnrichment.Ident, airportEnrichment.Type, airportEnrichment.Name, airportEnrichment.LatitudeDeg, airportEnrichment.LongitudeDeg, airportEnrichment.ElevationFt, airportEnrichment.Continent, airportEnrichment.IsoCountry, airportEnrichment.IsoRegion, airportEnrichment.Municipality, airportEnrichment.ScheduledService, airportEnrichment.GpsCode, airportEnrichment.IataCode, airportEnrichment.LocalCode, airportEnrichment.HomeLink, airportEnrichment.WikipediaLink, airportEnrichment.Keywords)
+		count, error := result.RowsAffected()
+		if error != nil {
+			log.Println(error)
+			log.Println("Failed to execute sql ", sqlStatement)
+		}
+		if count != 1 {
+			log.Println("Expected insert count to be 1, got ", count)
+		}
 
 		insertCount++
 
@@ -88,6 +97,7 @@ func UpdateAirportTable() {
 	`
 	result, error := Db.Exec(sqlStatement)
 	if error != nil {
+		log.Println(error)
 		log.Fatal("Failed to execute sql ", sqlStatement)
 	}
 	count, _ := result.RowsAffected()
@@ -146,168 +156,3 @@ func buildRow(columnNameToIndex map[string]int, i int, row []string) *AirportEnr
 
 	return &airportEnrichment
 }
-
-/*
-// GetUser function
-func GetUser() string {
-	var user string
-	var err error
-	if Db == nil {
-		log.Println("db is null")
-	}
-	err = Db.Get(&user, "select user from dual")
-	if err != nil {
-		log.Println(err)
-		return ""
-	}
-	log.Println(user)
-	return user
-}
-
-// SelectMetarListInObervationTimeRange function
-func SelectMetarListInObervationTimeRange(stationIDs []string, fromObservationTime time.Time, toObservationTime time.Time) []Metar {
-
-	log.Printf("stationIDs: %v %T, fromObservationTime: %v %T, toObservationTime: %v %T", stationIDs, stationIDs, fromObservationTime, fromObservationTime, toObservationTime, toObservationTime)
-	log.Printf("length of stationIDs: %v", len(stationIDs))
-
-	metarSlice := []Metar{}
-
-	const sqlStatement = `
-		select
-			station_id,
-			observation_time,
-			auto,
-			raw_text,
-			wind_dir_degrees,
-			wind_speed_kt,
-			wind_gust_kt,
-			visibility_statute_mi,
-			wx_string, sky_cover_1,
-			cloud_base_ft_agl_1,
-			sky_cover_2,
-			cloud_base_ft_agl_2,
-			sky_cover_3,
-			cloud_base_ft_agl_3,
-			sky_cover_4,
-			cloud_base_ft_agl_4,
-			vert_vis_ft,
-			temp_c,
-			dewpoint_c,
-			altim_in_hg
-		from metar
-		where
-			station_id in (?)
-			and observation_time >= ? and observation_time <= ?
-			order by station_id, observation_time`
-
-	query, args, err := sqlx.In(sqlStatement, stationIDs, fromObservationTime, toObservationTime)
-	if err != nil {
-		log.Println("Error in excuting sqlx.In()")
-		log.Println(err)
-		return nil
-	}
-	log.Println("After excuting sqlx.In()")
-	//query = Db.Rebind(query)
-	query = oracleRebind(query)
-	err = Db.Select(&metarSlice, query, args...)
-	if err != nil {
-		log.Println("Error in excuting Db.Select()")
-		log.Println(err)
-		return nil
-	}
-	log.Println("After excuting Db.Select()")
-	return metarSlice
-}
-
-// SelectMetarListForLatestNObservations function
-func SelectMetarListForLatestNObservations(stationIDs []string, latestNumberOfMetars string) []Metar {
-
-	log.Printf("stationIDs: %v %T, latestNumberOfMetars: %v %T", stationIDs, stationIDs, latestNumberOfMetars, latestNumberOfMetars)
-	log.Printf("length of stationIDs: %v", len(stationIDs))
-
-	metarSlice := []Metar{}
-
-	const sqlStatement = `
-		select
-			station_id,
-			observation_time,
-			auto,
-			raw_text,
-			wind_dir_degrees,
-			wind_speed_kt,
-			wind_gust_kt,
-			visibility_statute_mi,
-			wx_string,
-			sky_cover_1,
-			cloud_base_ft_agl_1,
-			sky_cover_2,
-			cloud_base_ft_agl_2,
-			sky_cover_3,
-			cloud_base_ft_agl_3,
-			sky_cover_4,
-			cloud_base_ft_agl_4,
-			vert_vis_ft,
-			temp_c,
-			dewpoint_c,
-			altim_in_hg
-		from (select
-				station_id,
-					observation_time,
-					auto,
-					raw_text,
-					wind_dir_degrees,
-					wind_speed_kt,
-					wind_gust_kt,
-					visibility_statute_mi,
-					wx_string,
-					sky_cover_1,
-					cloud_base_ft_agl_1,
-					sky_cover_2,
-					cloud_base_ft_agl_2,
-					sky_cover_3,
-					cloud_base_ft_agl_3,
-					sky_cover_4,
-					cloud_base_ft_agl_4,
-					vert_vis_ft,
-					temp_c,
-					dewpoint_c,
-					altim_in_hg,
-					rank() over (partition by station_id order by observation_time desc) as observation_time_rank
-				from metar
-				where station_id in (?)
-					and observation_time >= sysdate-3
-			)
-	where observation_time_rank <= ? order by station_id, observation_time`
-
-	query, args, err := sqlx.In(sqlStatement, stationIDs, latestNumberOfMetars)
-	if err != nil {
-		log.Println("Error in excuting sqlx.In()")
-		log.Println(err)
-		return nil
-	}
-	log.Println("After excuting sqlx.In()")
-
-	//query = Db.Rebind(query)
-	query = oracleRebind(query)
-
-	err = Db.Select(&metarSlice, query, args...)
-	if err != nil {
-		log.Println("Error in excuting Db.Select()")
-		log.Println(err)
-		return nil
-	}
-	log.Println("After excuting Db.Select()")
-	log.Print(len(metarSlice))
-	return metarSlice
-}
-
-// Replaces ? with :n bind placeholder
-func oracleRebind(sqlStatement string) string {
-	var i = 0
-	for strings.Index(sqlStatement, "?") > -1 {
-		i++
-		sqlStatement = strings.Replace(sqlStatement, "?", ":"+strconv.Itoa(i), 1)
-	}
-	return sqlStatement
-}
-*/
